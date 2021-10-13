e35b5896b2c413a9b873b4df7fb3bf43c8cdc731
Jean-César <jeancesar.bazin@gmail.com>
2021-10-13 12:12:49 +0200

    first commit

---
+++ src/main/java/Mediscreenmsnotes/MediscreenMsNotesApplication.java
@@ -0,0 +1,13 @@
+package Mediscreenmsnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenMsNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenMsNotesApplication.class, args);
+	}
+
+}
